bb2e25f3e4daad24b9906e782b1f96fc0784562e
Kyle Shockey <shockey@users.noreply.github.com>
2019-03-19 22:12:48 -0700

    add top interactor `--until` CLI option

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 1. download a repository's comments: `node fetch-data [repo_owner] [repo_name]`
 2. confirm data integrity: `node check-data [repo_owner] [repo_name]`
-3. generate a report: `node report-top-interactors [repo_owner] [repo_name] --since [timestamp] --cutoff [minimum_score]`
+3. generate a report: `node report-top-interactors [repo_owner] [repo_name] --since [timestamp] --until [timestamp] --cutoff [minimum_score]`
 
 Reports, by default, only consider issues/PRs/comments created within the last two years. This can be overridden with the `--since` option.
 
--- report-top-interactors.js
+++ report-top-interactors.js
@@ -11,6 +11,12 @@ const argv = require('yargs').argv
 
 const CUTOFF = argv.cutoff || 5
 const SINCE = argv.since ? moment(argv.since) : moment().subtract(2, "years")
+const UNTIL = argv.until ? moment(argv.until) :  moment()
+
+if(UNTIL.isSameOrBefore(SINCE)) {
+  console.error("-> Fatal error: `until` is before or the same as `since`. Can't compute anything!")
+  process.exit(1)
+}
 
 const [TARGET_OWNER, TARGET_REPO] = process.argv.slice(2)
 
@@ -30,14 +36,17 @@ const increment = (author, item) => {
 
 repoData.pulls
   .filter(pull => SINCE.isBefore(pull.created_at))
+  .filter(pull => UNTIL.isSameOrAfter(pull.created_at))
   .forEach(pull => increment(pull.user.login, "pulls"))
 
 repoData.issues
   .filter(issue => SINCE.isBefore(issue.created_at))
+  .filter(issue => UNTIL.isSameOrAfter(issue.created_at))
   .forEach(issue => increment(issue.user.login, "issues"))
 
 repoData.comments
   .filter(comment => SINCE.isBefore(comment.created_at))
+  .filter(comment => UNTIL.isSameOrAfter(comment.created_at))
   .forEach(comment => increment(comment.user.login, "comments"))
 
 let tuples = []
@@ -54,7 +63,7 @@ tuples.sort((a, b) => {
 
 tuples = tuples.filter(([author, score]) => score >= CUTOFF)
 
-console.log(`-- Top Interactors for ${TARGET_OWNER}/${TARGET_REPO} since ${SINCE.format("MMMM D YYYY")} --`)
+console.log(`-- Top interactors for ${TARGET_OWNER}/${TARGET_REPO} from ${SINCE.format("M/D/YY")} to ${UNTIL.format("M/D/YY")} --`)
 tuples.forEach(([user, score]) => {
   score = score.toString()
   const dataForUser = reportData[user]
